d2e8f26c3a662004b75c93e515166f1d7aa49b93
prograrnina <programaharnina2013@gmail.com>
2017-10-05 14:00:33 +0200

    cc

---
+++ Imaginaria v2/src/Imaginaria/Seres.java
@@ -0,0 +1,48 @@
+package Imaginaria;
+
+public class Seres extends Thread {
+
+	int vida;
+	int tamano = 0;
+	Lago lago;
+	String nombre;
+
+	public Seres(String nombre,Lago lago,int vida) {
+		this.lago=lago;
+		this.vida=vida;
+		this.nombre=nombre;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public int getTamano() {
+		return tamano;
+	}
+
+	public void setTamano(int tamano) {
+		this.tamano = tamano;
+	}
+
+	@Override
+	public String toString() {
+		return "tiene de vida " + vida + " y de tamano " + tamano;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Llego aqui");
+		while (tamano < vida) {
+			lago.beber();
+			tamano++;
+			System.out.println(nombre);
+		}
+		System.out.println("muriendo con tamaño "+tamano);
+	}
+	
+}
