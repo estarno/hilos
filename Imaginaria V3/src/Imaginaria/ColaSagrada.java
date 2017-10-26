e82c6e0bd8051d407d3ce3d24292a7ff0b6cb4f9
CicloDAMHarnina <programaharnina2013@gmail.com>
2017-10-26 12:33:11 +0200

    final

--- Imaginaria V3/src/Imaginaria/ColaSagrada.java
+++ Imaginaria V3/src/Imaginaria/ColaSagrada.java
@@ -3,36 +3,41 @@ package Imaginaria;
 public class ColaSagrada extends Thread {
 	private Cola fila = new Cola();
 	private Lago lagoSagrado = Lago.getInstance();
-	private boolean vaciada=false;
+	private boolean vaciada = false;
 	// Singleton
 	private static ColaSagrada instancia = new ColaSagrada();
 
-	private ColaSagrada() {	
+	private ColaSagrada() {
 	}
+
 	public static ColaSagrada getInstance() {
 		return instancia;
 	}
-	
+
 	public void solicitarPuesto(Seres ser) {
 		fila.ingresarCola(ser);
 	}
-	
+
 	@Override
 	public void run() {
 		super.run();
-		while(true&&!vaciada){
-			//ahora es la cola la que gestiona si se bebe del lago
-			//lago es sincro y por lo tanto para el hilo de la cola
-			//si no se puede beber
+		// while(true&&!vaciada){
+		while (true) {
+			// ahora es la cola la que gestiona si se bebe del lago
+			// lago es sincro y por lo tanto para el hilo de la cola
+			// si no se puede beber
 			lagoSagrado.beber();
-			//cuando se bebe obtengo el elemento de cola que ha bebido
-			Seres ser=fila.consumirTurno();
-			if(ser!=null){
-				//le digo al ser que ha bebido
+			// cuando se bebe obtengo el elemento de cola que ha bebido
+			Seres ser = fila.consumirTurno();
+			if (ser != null) {
+				// le digo al ser que ha bebido
 				ser.beber();
 				System.out.println("se esta alimentando " + ser.getName());
 			}
-			vaciada=fila.isVacia();
+			// vaciada=fila.isVacia();
+			// System.out.println(vaciada);
+			if (fila.isVacia())
+				System.out.println(lagoSagrado.getVida());
 		}
 	}
 }
